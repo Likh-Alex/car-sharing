c59963e3f1b96bcce2383657adfffff4be0819d0
Likh-Alex <likhachov991@protonmail.com>
2021-01-12 21:23:58 +0200

    Solved the task

--- src/main/java/taxi/exception/NoSuchManufacturerException.java
+++
@@ -1,7 +0,0 @@
-package taxi.exception;
-
-public class NoSuchManufacturerException extends RuntimeException {
-    public NoSuchManufacturerException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/taxi/service/impl/DriverServiceImpl.java
@@ -0,0 +1,41 @@
+package taxi.service.impl;
+
+import java.util.List;
+import taxi.dao.DriverDao;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+
+@Service
+public class DriverServiceImpl implements DriverService {
+    @Inject
+    private DriverDao driverDao;
+
+    @Override
+    public Driver create(Driver driver) {
+        driverDao.create(driver);
+        return driver;
+    }
+
+    @Override
+    public Driver get(Long id) {
+        return driverDao.get(id).orElseThrow(()
+                -> new RuntimeException("No such driver stored with id : " + id));
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return driverDao.getAll();
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return driverDao.update(driver);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return driverDao.delete(id);
+    }
+}
