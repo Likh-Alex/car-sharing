d7b22bb4545368ad65e74187bc5ac815ee3e993f
Likh-Alex <likhachov991@protonmail.com>
2021-01-12 12:27:14 +0200

    - Code style refactoring - refactored method `update` to use `for i` loop with `List.set()` - refactored delete to use `List.removeIf()` method

--- src/main/java/taxi/dao/impl/ManufacturerDaoImpl.java
+++ src/main/java/taxi/dao/impl/ManufacturerDaoImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import taxi.dao.ManufacturerDao;
 import taxi.db.Storage;
-import taxi.exception.NoSuchManufacturerException;
 import taxi.lib.Dao;
 import taxi.model.Manufacturer;
 
@@ -20,7 +19,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     public Optional<Manufacturer> get(Long id) {
         return Storage.manufacturers
                 .stream()
-                .filter(man -> man.getId().equals(id))
+                .filter(m -> m.getId().equals(id))
                 .findAny();
     }
 
@@ -31,27 +30,17 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
 
     @Override
     public Manufacturer update(Manufacturer manufacturer) {
-        Optional<Manufacturer> targetManufacturer = get(manufacturer.getId());
-        for (Manufacturer storageManufacturer : Storage.manufacturers) {
-            if (targetManufacturer.isPresent()) {
-                if (storageManufacturer.getId().equals(targetManufacturer.get().getId())) {
-                    storageManufacturer.setCountry(manufacturer.getCountry());
-                    storageManufacturer.setName(manufacturer.getName());
-                }
+        for (int i = 0; i < Storage.manufacturers.size(); i++) {
+            Manufacturer storageManufacturer = Storage.manufacturers.get(i);
+            if (manufacturer.getId().equals(storageManufacturer.getId())) {
+                Storage.manufacturers.set(i, manufacturer);
             }
         }
-        return targetManufacturer.orElseThrow(()
-                -> new NoSuchManufacturerException("No manufacturer with name: "
-                + manufacturer.getName()));
+        return manufacturer;
     }
 
     @Override
     public boolean delete(Long id) {
-        Optional<Manufacturer> manufacturer = get(id);
-        if (manufacturer.isPresent()) {
-            Storage.manufacturers.remove(manufacturer.get());
-            return true;
-        }
-        return false;
+        return Storage.manufacturers.removeIf(m -> m.getId().equals(id));
     }
 }
